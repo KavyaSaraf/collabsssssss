c7a2dd5d7e4fa900a7df0031d82449b071099d0f
KavyaSaraf <sarafkavya29@gmail.com>
2024-12-09 11:05:56 +0530

    linting and type off

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
       },
     ],
   },
+  eslint: {
+    // Disable linting during the build process
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Disable type-checking during the build process
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
